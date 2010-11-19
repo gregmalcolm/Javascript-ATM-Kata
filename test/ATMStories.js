8b79f46a214aa77151625babf6182df6922a1a09
Zachary Spencer <zspencer@zacharyspencer.com>
2010-11-19 15:48:03 -0500

    Switched out from unit level testing to story through the UI level testing.

--- test/ATMStories.js
+++ test/ATMStories.js
@@ -1,30 +1,42 @@
-describe("ATM Story Tests", function() {
-    var machine;
+describe("ATM Story Tests", function(){
     beforeEach(function() {
-         var accounts = {
-            '1234567890': {
-                holder: 'Judy',
-                balance: 100,
-                pin: 1234
-            }
-        }
-        machine = ATM.create(accounts);
-
+        // Put however you reset everything in here.
+        // This should reset Judy's account balance too.
+        
     });
     it("Allows Judy to withdraw money when she has a balance available", function() {
-        expect(machine.withdraw(10, '1234567890', 1234)).toEqual(10);
+        $('input#accountNumber').val('1234567890');
+        $('input#PIN').val('1234');
+        $('input#amount').val(10);
+        $('input#withdraw').click();
+        expect($('#cashDrawer').val()).toEqual(10);
     });
     it("Prevents Judy from withdrawing money when she doesn't have it available", function() {
-        expect(machine.withdraw(105, '1234567890', 1234)).toEqual(0);
+        $('input#accountNumber').val('1234567890');
+        $('input#PIN').val('1234');
+        $('input#amount').val(110);
+        $('input#withdraw').click();
+        expect($('#cashDrawer').val()).toEqual(0);
     });
     it("Prevents Judy from withdrawing money when she puts in an incorrect pin", function() {
-        expect(machine.withdraw(50, '1234567890', 5678)).toEqual(0);
-    });
+        $('input#accountNumber').val('1234567890');
+        $('input#PIN').val('5678');
+        $('input#amount').val(25);
+        $('input#withdraw').click();
+        expect($('#cashDrawer').val()).toEqual(0);
+ });
     it("Prevents Judy from withdrawing money when she puts in an incorrect account number", function() {
-        expect(machine.withdraw(50, '0987654321', 1234)).toEqual(0);
-    });
+        $('input#accountNumber').val('0987654321');
+        $('input#PIN').val('1234');
+        $('input#amount').val(25);
+        $('input#withdraw').click();
+        expect($('#cashDrawer').val()).toEqual(0);
+   });
     it("Updates Judys balance when she withdraws money", function() {
-        machine.withdraw(25, '1234567890', 1234);
-        expect(machine.checkBalance('1234567890', 1234).toEqual(75));
+        $('input#accountNumber').val('0987654321');
+        $('input#PIN').val('1234');
+        $('input#amount').val(10);
+        $('input#withdraw').click();
+        expect($('#accountBalance').text()).toEqual(90);
     });
 });
