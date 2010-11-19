0bb4dbd7c72afc00c3920bfcece1759761ede02b
Zachary Spencer <zspencer@zacharyspencer.com>
2010-11-19 14:30:15 -0500

    Created the initial stories

---
+++ src/ATM.js
@@ -0,0 +1,13 @@
+var ATM = (function() {
+    var ATM = function() {}
+    ATM.prototype = {
+        withdraw: function() {
+
+        }
+    }
+    return {
+        create: function() {
+            return new ATM();
+        }
+    }
+})();
---
+++ test/ATMStories.js
@@ -0,0 +1,26 @@
+describe("ATM Story Tests", function() {
+    var machine;
+    beforeEach(function() {
+         var accounts = {
+            '1234567890': {
+                holder: 'Judy',
+                balance: 100,
+                pin: 1234
+            }
+        }
+        machine = ATM.create(accounts);
+
+    });
+    it("Allows Judy to withdraw money when she has a balance available", function() {
+        expect(machine.withdraw(10, '1234567890', 1234)).toEqual(10);
+    });
+    it("Prevents Judy from withdrawing money when she doesn't have it available", function() {
+        expect(machine.withdraw(105, '1234567890', 1234)).toEqual(0);
+    });
+    it("Prevents Judy from withdrawing money when she puts in an incorrect pin", function() {
+        expect(machine.withdraw(50, '1234567890', 5678)).toEqual(0);
+    });
+    it("Prevents Judy from withdrawing money when she puts in an incorrect account number", function() {
+        expect(machine.withdraw(50, '0987654321', 1234)).toEqual(0);
+    });
+});
